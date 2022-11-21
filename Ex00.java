5a303a5dbffc64e2b6528e2ec22d9d17a2265d39
Otávio Segawa <95499529+OtavioSegawa@users.noreply.github.com>
2022-11-21 10:23:36 -0300

    Add files via upload

---
+++ Ex00.java
@@ -0,0 +1,13 @@
+/*
+Ex00: Fazer o Olá Mundo!!! para quebrar a maldição
+Professor: Gustavo Guanabara
+Plataforma: EstudoNauta
+Aluno: Otávio Shoity Segawa Goya
+ */
+public class CursoJavaBasico {
+    public static void main(String[] args) {
+        System.out.println("------------------");
+        System.out.println("\tOlá Mundo!");
+        System.out.println("------------------");
+    }
+}
